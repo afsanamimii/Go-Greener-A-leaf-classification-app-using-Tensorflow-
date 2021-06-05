cd9f37431a794f5ab4dffa14f997e46ebcd47b4c
Afsana Mimi <61627644+afsanamimii@users.noreply.github.com>
2021-06-05 10:05:03 +0600

    Files Upload

---
+++ lite/codelabs/flower_classification/android/start/app/src/main/java/org/tensorflow/lite/examples/classification/splashscreen.java
@@ -0,0 +1,30 @@
+package org.tensorflow.lite.examples.classification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(splashscreen.this, ClassifierActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },4000);
+
+
+
+
+    }
+}
+
